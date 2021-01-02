8f5cd51a76bbf868271a1d6f591637fc403d5d52
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-01-02 03:46:02 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner
+class javapractice {
+    static void main(String[] args) {
+        System.out.println("Welcome to MrBehemoths Console Application" +
+                "This application was built in Java")
+        System.out.println("Specify a race to get started")
+    }
+
+}
+import java.util.Scanner;
+class userinput {
+    static void main(args) {
+        Scanner scanTime = new Scanner(System.in);
+        Scanner scanArgs = new Scanner(System.in)
+        System.out.println("Enter a distance in meters or enter custom for custom values")
+        int scanTimeval = scanTime.nextInt()
+        String scanArgsval = scanArgs.nextLine()
+
+        if (scanTimeval == 1600) {
+            System.out.println("My mile personal record is 5:19")
+        }
+        if (scanTimeval == 3200) {
+            System.out.println("My 2 mile personal record is 12:04")
+        }
+        if (scanTimeval == 5000) {
+            System.out.println("My 5K personal record is 19:09")
+        }
+        if (scanTimeval == 10000) {
+            System.out.println("My 10K personal record is 45:22")
+        }
+        if (scanArgsval == "custom") {
+            System.out.println("Enter your mile best")
+            Scanner scanMile = new Scanner(System.in)
+            int scanMilerace = scanMile.nextLine()
+            System.out.println(scanMilerace)
+
+            System.out.println("Enter your 2 mile best")
+            Scanner scanTwomile = new Scanner(System.in)
+            int scanTwomilerace = scanTwomile.nextLine()
+            System.out.println(scanTwomilerace)
+
+            System.out.println("Enter your 5K best")
+            Scanner scanFivek = new Scanner(System.in)
+            int scanFivekrace = scanFivek.nextLine()
+            System.out.println(scanFivekrace)
+
+            System.out.println("Enter your 10K best")
+            Scanner scanTenk = new Scanner(System.in)
+            int scanTenkrace = scanTenk.nextLine()
+            System.out.println(scanTenkrace)
+        }
+    }
+}
